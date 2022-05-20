e915c463103a76e03f633e8c23ec3ca9d04d7111
Kov <guillaume.koltok@gmail.com>
2022-05-20 15:45:42 +0200

    ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    str = string.toLowerCase().replace(/[^\w\s]/g, "").replace(/\s/g, '');
+    console.log(str);
+    console.log(Math.floor(str.length/2));
+    let middle = parseInt(Math.floor(str.length/2));
+    middleNeg = -Math.abs(middle);
+    console.log(middleNeg)
+    firstPart = str.slice(0, middle);
+    secondPart = str.slice(middleNeg);
+    console.log(firstPart);
+    secondPart = secondPart.split("").reverse().join("");
+    console.log(secondPart);
+    if(firstPart === secondPart){
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
